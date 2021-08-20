d5130a51e08229466f2dca7766726a7125c0ab36
Kumar Desappan <kumar.adk@gmail.com>
2021-08-20 20:25:25 +0530

    [CPP BUILD]Validated CPP example build on target
    Validated one example (TFL)

--- examples/cmake/common.cmake
+++ examples/cmake/common.cmake
@@ -9,10 +9,20 @@ IF(NOT CMAKE_BUILD_TYPE)
   SET(CMAKE_BUILD_TYPE Release)
 ENDIF()
 
+if(NOT DEFINED CMAKE_SYSTEM_PROCESSOR OR CMAKE_SYSTEM_PROCESSOR STREQUAL "")
+  message(WARNING "CMAKE_SYSTEM_PROCESSOR is not defined. Perhaps CMake toolchain is broken")
+endif()
+
+message(STATUS "Detected processor: ${CMAKE_SYSTEM_PROCESSOR}")
+if(CMAKE_SYSTEM_PROCESSOR MATCHES "amd64.*|x86_64.*|AMD64.*")
+  set(X86_64 1)
+elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "^(aarch64.*|AARCH64.*|arm64.*|ARM64.*)")
+  set(AARCH64 1)
+endif()
+
+
 message(STATUS "CMAKE_BUILD_TYPE = ${CMAKE_BUILD_TYPE} PROJECT_NAME = ${PROJECT_NAME}")
 
-set(CMAKE_C_COMPILER gcc-5)
-set(CMAKE_CXX_COMPILER g++-5)
 
 SET(CMAKE_FIND_LIBRARY_PREFIXES "" "lib")
 SET(CMAKE_FIND_LIBRARY_SUFFIXES ".a" ".lib" ".so")
@@ -62,13 +72,20 @@ add_definitions(
     -DTARGET_OS=${TARGET_OS}
 )
 
-link_directories(/usr/local/dlr
+link_directories(/usr/lib 
+                 /usr/local/dlr
                  /usr/lib/aarch64-linux-gnu
-                 /usr/lib/
-                 ${OPENCV_INSTALL_DIR}/cmake/lib
+                 /usr/lib/python3.8/site-packages/dlr/
+                 )
+
+if(X86_64 EQUAL 1)
+link_directories(${OPENCV_INSTALL_DIR}/cmake/lib
                  ${OPENCV_INSTALL_DIR}/cmake/3rdparty/lib
                  $ENV{TIDL_TOOLS_PATH}
                  )
+set(CMAKE_C_COMPILER gcc-5)
+set(CMAKE_CXX_COMPILER g++-5)
+endif()             
 
 #message("PROJECT_SOURCE_DIR =" ${PROJECT_SOURCE_DIR})
 #message("CMAKE_SOURCE_DIR =" ${CMAKE_SOURCE_DIR})
@@ -103,6 +120,8 @@ include_directories(${PROJECT_SOURCE_DIR}
 #    common
 #    )
 
+
+if(X86_64 EQUAL 1)
 set(SYSTEM_LINK_LIBS
     glib-2.0
     gobject-2.0
@@ -123,7 +142,21 @@ set(SYSTEM_LINK_LIBS
     pthread
     dl
     )
-
+else()
+set(SYSTEM_LINK_LIBS
+    glib-2.0
+    gobject-2.0
+    opencv_imgproc
+    opencv_imgcodecs
+    opencv_core
+    dlr
+    tensorflow-lite
+    onnxruntime
+    vx_tidl_rt
+    pthread
+    dl
+    )
+endif()  
 
 # Function for building a node:
 # ARG0: app name
--- setup.sh
+++ setup.sh
@@ -31,6 +31,31 @@
 ######################################################################
 SCRIPTDIR=`pwd`
 
+
+skip_cpp_deps=0
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    --skip_cpp_deps)
+    skip_cpp_deps=1
+    ;;
+    -h|--help)
+    echo Usage: $0 [options]
+    echo
+    echo Options,
+    echo --skip_cpp_deps            Skip Downloading or Compiling dependencies for CPP examples
+    exit 0
+    ;;
+esac
+shift # past argument
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+
+
 version_match=`python3 -c 'import sys;r=0 if sys.version_info >= (3,6) else 1;print(r)'`
 if [ $version_match -ne 0 ]; then
 echo 'python version must be >= 3.6'
@@ -49,12 +74,18 @@ wget https://github.com/TexasInstruments/edgeai-tidl-tools/releases/download/08.
 tar -xzf tidl_tools.tar.gz
 rm tidl_tools.tar.gz
 cd  tidl_tools
-wget https://github.com/TexasInstruments/edgeai-tidl-tools/releases/download/08.00.00-rc2/libonnxruntime.so.1.7.0
-wget https://github.com/TexasInstruments/edgeai-tidl-tools/releases/download/08.00.00-rc2/libtensorflow-lite.a
-ln -s libonnxruntime.so.1.7.0 libonnxruntime.so
+    if [ $skip_cpp_deps -eq 0 ]
+    then
+        wget https://github.com/TexasInstruments/edgeai-tidl-tools/releases/download/08.00.00-rc2/libonnxruntime.so.1.7.0
+        wget https://github.com/TexasInstruments/edgeai-tidl-tools/releases/download/08.00.00-rc2/libtensorflow-lite.a
+        ln -s libonnxruntime.so.1.7.0 libonnxruntime.so
+    fi
 export TIDL_TOOLS_PATH=$(pwd)
 export LD_LIBRARY_PATH=$TIDL_TOOLS_PATH
 cd ..
+
+if [ $skip_cpp_deps -eq 0 ]
+then
 cd ..
 git clone --depth 1 --single-branch -b tidl-j7 https://github.com/TexasInstruments/neo-ai-dlr
 git clone --depth 1 --single-branch -b tidl-j7 https://github.com/TexasInstruments/onnxruntime.git
@@ -73,6 +104,8 @@ cd opencv-4.1.0/cmake/
 cmake -DBUILD_opencv_highgui:BOOL="1" -DBUILD_opencv_videoio:BOOL="0" -DWITH_IPP:BOOL="0" -DWITH_WEBP:BOOL="1" -DWITH_OPENEXR:BOOL="1" -DWITH_IPP_A:BOOL="0" -DBUILD_WITH_DYNAMIC_IPP:BOOL="0" -DBUILD_opencv_cudacodec:BOOL="0" -DBUILD_PNG:BOOL="1" -DBUILD_opencv_cudaobjdetect:BOOL="0" -DBUILD_ZLIB:BOOL="1" -DBUILD_TESTS:BOOL="0" -DWITH_CUDA:BOOL="0" -DBUILD_opencv_cudafeatures2d:BOOL="0" -DBUILD_opencv_cudaoptflow:BOOL="0" -DBUILD_opencv_cudawarping:BOOL="0" -DINSTALL_TESTS:BOOL="0" -DBUILD_TIFF:BOOL="1" -DBUILD_JPEG:BOOL="1" -DBUILD_opencv_cudaarithm:BOOL="0" -DBUILD_PERF_TESTS:BOOL="0" -DBUILD_opencv_cudalegacy:BOOL="0" -DBUILD_opencv_cudaimgproc:BOOL="0" -DBUILD_opencv_cudastereo:BOOL="0" -DBUILD_opencv_cudafilters:BOOL="0" -DBUILD_opencv_cudabgsegm:BOOL="0" -DBUILD_SHARED_LIBS:BOOL="0" -DWITH_ITT=OFF ../
 make -j 32
 cd -
+fi
+
 cd $SCRIPTDIR
 
 
