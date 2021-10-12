bd17319eb67ca8180e9f880d232dc78c0ec149b5
Paula Carrillo <a0133185@ti.com>
2021-10-12 16:58:23 -0500

    Adding loggerWritter function and High Resolution to utils.py - Modified custom-model-tfl to save logs

--- examples/jupyter_notebooks/scripts/utils.py
+++ examples/jupyter_notebooks/scripts/utils.py
@@ -17,6 +17,8 @@ from jai_benchmark.sessions.onnxrt_session import ONNXRTSession
 from jai_benchmark.utils.artifacts_id_to_model_name import model_id_artifacts_pair
 
 import os
+import sys
+from pathlib import Path
 import platform
 
 if platform.machine() != 'aarch64':
@@ -343,7 +345,7 @@ def get_benchmark_output(benchmark_dict):
 
     total_time = total_time - copy_time
 
-    return total_time/1000000, proc_time/1000000, write_total/1000000, read_total/1000000
+    return total_time/1000000, proc_time/1000000, read_total/1000000, write_total/1000000
 
 colors_list = [
 ( 255, 	 0,	  0 ), ( 0	 , 255,    0 ), ( 0	,   0,	 255 ), ( 255, 255,	    0  ), ( 0	 , 255,  255  ), ( 255,   0,	 255  ),
@@ -474,7 +476,7 @@ def get_selected_artifacts_id():
             break
     return result_artifacts_key   
 
-def get_eval_configs(task_type, runtime_type, num_quant_bits, last_artifacts_id=None):
+def get_eval_configs(task_type, runtime_type, num_quant_bits, last_artifacts_id=None, high_resolution = False):
     
     if runtime_type == 'tflitert':
         session_type = TFLiteRTSession
@@ -489,15 +491,45 @@ def get_eval_configs(task_type, runtime_type, num_quant_bits, last_artifacts_id=
         session_type_dict = {'onnx': 'onnxrt', 'tflite': 'tflitert', 'mxnet': 'tvmdlr'}
 
     prebuilts_dir = 'prebuilt-models'
-    settings = ConfigSettings({
-    'dataset_loading' : False,
-    'tidl_tensor_bits' : num_quant_bits,
-    'task_selection' : task_type,
-    'session_type_dict' : session_type_dict,
-    'run_import' : False
-    })
+
+    settings_dict = {
+        'dataset_loading' : False,
+        'tidl_tensor_bits' : num_quant_bits,
+        'task_selection' : task_type,
+        'session_type_dict' : session_type_dict,
+        'run_import' : False
+    }
+
+    # This option changes the input sizes of these selected models to high resolution
+    # This is only used for performance measurement. For more details, refer to:
+    # https://github.com/TexasInstruments/edgeai-benchmark/blob/master/scripts/benchmark_resolution.py
+    if high_resolution:
+        # only these models are supported in high_resolution setting
+        model_selection_high_resolution = [
+                           'edgeai-tv/mobilenet_v1_20190906.onnx',
+                           'edgeai-tv/mobilenet_v2_20191224.onnx',
+                           'edgeai-tv/mobilenet_v2_1p4_qat-p2_20210112.onnx',
+                           'torchvision/resnet18.onnx',
+                           'torchvision/resnet50.onnx',
+                           'fbr-pycls/regnetx-400mf.onnx',
+                           'fbr-pycls/regnetx-800mf.onnx',
+                           'fbr-pycls/regnetx-1.6gf.onnx'
+                          ]
+        # these artifacts are meant for only performance measurement - just do a quick import with simple calibration
+        # also set the high_resolution_optimization flag for improved performance at high resolution
+        runtime_options = {'accuracy_level': 0, 'advanced_options:high_resolution_optimization': 1}
+        # the transformations that needs to be applied to the model itself. Note: this is different from pre-processing transforms
+        high_resolution_input_sizes = [512, 1024]
+        model_transformation_dict = {'input_sizes': high_resolution_input_sizes}
+        settings_update_high_resolution = dict(model_selection=model_selection_high_resolution,
+            num_frames=100, calibration_iterations=1, runtime_options=runtime_options,
+            model_transformation_dict=model_transformation_dict)
+        settings_dict.update(settings_update_high_resolution)
+    #
+    settings = ConfigSettings(settings_dict)
     prebuilt_configs = select_configs(settings,os.path.join(prebuilts_dir, f'{num_quant_bits}bits'), runtime_type)
     merged_list = get_name_key_pair_list(prebuilt_configs.keys(), runtime_type)
+    #print("merged_list: ", prebuilt_configs.keys())
 
     model_selection_artifacts_key = get_selected_artifacts_id()
     if not model_selection_artifacts_key is None:
@@ -529,3 +561,46 @@ def get_preproc_props(pipeline_config):
 def task_type_to_dataset_list(task_type):
     assert task_type == 'classification'
     return ['imagenet']
+
+"""
+class loggerWritter():
+    # Redirect c- stdout and stderr to a couple of files.
+"""
+
+log_dir = Path('./')
+
+class loggerWritter():
+    def __init__(self, logname):
+        self.logname = logname
+        sys.stdout.flush()
+        sys.stderr.flush()
+
+        if self.logname == None:
+            self.logpath_out = os.devnull
+            self.logpath_err = os.devnull
+        else:
+            self.logpath_out = log_dir / (logname + "_out.log")
+            self.logpath_err = log_dir / (logname + "_err.log")
+
+        self.logfile_out = os.open(self.logpath_out, os.O_WRONLY|os.O_TRUNC|os.O_CREAT)
+        self.logfile_err = os.open(self.logpath_err, os.O_WRONLY|os.O_TRUNC|os.O_CREAT)
+    def __enter__(self):
+        self.orig_stdout = sys.stdout # save original stdout
+        self.orig_stderr = sys.stderr # save original stderr
+
+        self.new_stdout = os.dup(1)
+        self.new_stderr = os.dup(2)
+
+        os.dup2(self.logfile_out, 1)
+        os.dup2(self.logfile_err, 2)
+
+        sys.stdout = os.fdopen(self.new_stdout, 'w')
+        sys.stderr = os.fdopen(self.new_stderr, 'w')
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.flush()
+        sys.stderr.flush()
+
+        sys.stdout = self.orig_stdout # restore original stdout
+        sys.stderr = self.orig_stderr # restore original stderr
+        os.close(self.logfile_out)
+        os.close(self.logfile_err)
