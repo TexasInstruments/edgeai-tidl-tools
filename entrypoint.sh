b1ea8c05d61dfeb4a6ea74df28bc8b2f6dd42d46
Kumar Desappan <kumar.adk@gmail.com>
2022-03-10 15:43:02 +0530

    CPP examples test in Github actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,10 @@
 
 time=$(date)
 echo "Current time is $time" > report.txt 
-source ./setup.sh --skip_cpp_deps
+source ./setup.sh
+mkdir build && cd build
+cmake ../examples
+cd ..
 source ./scripts/run_python_examples.sh
 python3 ./scripts/gen_test_report.py
 ls -l
