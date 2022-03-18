3f3e1deff9006f3bd36060569bcf691754100598
Manu Mathew <a0393608@udtensorlab5>
2022-03-18 09:50:52 +0530

    setup.py of osrt-model-tools updated to include package data

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, Extension
+from setuptools import setup, find_packages
 
 setup(
     name='osrt_model_tools',
-    packages=['osrt_model_tools'],
+    packages=find_packages(),
     install_requires=['setuptools>=18.0',],
     version='1.0',
 )
