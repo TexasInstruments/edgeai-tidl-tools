369b73bba012f46e1f242342ff19b532f1b7a8ef
Fadil Muhammed <a0496663@tidl-ud-03>
2022-05-31 14:08:25 +0530

    setup script upgrade

--- setup.sh
+++ setup.sh
@@ -29,6 +29,138 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ######################################################################
+download_compile_tflite_2.8(){
+    cd $HOME
+    mkdir tensorflow    
+    cd tensorflow
+    export TENSORFLOW_INSTALL_DIR=$(pwd)
+    git clone --depth 1 --single-branch -b v2.8.0 https://github.com/tensorflow/tensorflow.git tensorflow_src
+    
+    #native compialtion for x86
+    mkdir tflite_build
+    cd tflite_build
+    cmake ../tensorflow_src/tensorflow/lite
+    cmake --build . -j
+    
+    #cross compilation for arm
+    mkdir tflite_build_arm
+    cd tflite_build_arm
+    ARMCC_PREFIX=$ARM64_GCC_PATH/bin/aarch64-none-linux-gnu-    
+    ARMCC_FLAGS="-funsafe-math-optimizations"
+    cmake -DCMAKE_C_COMPILER=${ARMCC_PREFIX}gcc   -DCMAKE_CXX_COMPILER=${ARMCC_PREFIX}g++   -DCMAKE_C_FLAGS="${ARMCC_FLAGS}"   -DCMAKE_CXX_FLAGS="${ARMCC_FLAGS}"   -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON   -DCMAKE_SYSTEM_NAME=Linux  -DTFLITE_ENABLE_XNNPACK=ON  -DCMAKE_SYSTEM_PROCESSOR=aarch64   ../tensorflow_src/tensorflow/lite/
+    cmake --build . -j    
+}
+
+download_tflite2.4(){
+    cd $HOME
+    git clone --depth 1 --single-branch -b tidl-j7 https://github.com/TexasInstruments/onnxruntime.git
+    git clone --depth 1 --single-branch -b tidl-j7 https://github.com/TexasInstruments/tensorflow.git
+    mkdir -p tensorflow/tensorflow/lite/tools/make/downloads
+    cd tensorflow/tensorflow/lite/tools/make/downloads
+    wget https://github.com/google/flatbuffers/archive/v1.12.0.tar.gz
+    tar -xzf v1.12.0.tar.gz
+    rm v1.12.0.tar.gz
+    mv  flatbuffers-1.12.0 flatbuffers
+}
+
+download_compile_opencv(){
+    cd $HOME
+    wget https://github.com/opencv/opencv/archive/4.1.0.zip
+    unzip 4.1.0.zip
+    rm 4.1.0.zip
+    cp -r opencv-4.1.0/cmake opencv-4.1.0/cmake_static 
+    if [[ $arch == x86_64 ]]; then
+        cd opencv-4.1.0/cmake/
+        cmake -DBUILD_opencv_highgui:BOOL="1" -DBUILD_opencv_videoio:BOOL="0" -DWITH_IPP:BOOL="0" -DWITH_WEBP:BOOL="1" -DWITH_OPENEXR:BOOL="1" -DWITH_IPP_A:BOOL="0" -DBUILD_WITH_DYNAMIC_IPP:BOOL="0" -DBUILD_opencv_cudacodec:BOOL="0" -DBUILD_PNG:BOOL="1" -DBUILD_opencv_cudaobjdetect:BOOL="0" -DBUILD_ZLIB:BOOL="1" -DBUILD_TESTS:BOOL="0" -DWITH_CUDA:BOOL="0" -DBUILD_opencv_cudafeatures2d:BOOL="0" -DBUILD_opencv_cudaoptflow:BOOL="0" -DBUILD_opencv_cudawarping:BOOL="0" -DINSTALL_TESTS:BOOL="0" -DBUILD_TIFF:BOOL="1" -DBUILD_JPEG:BOOL="1" -DBUILD_opencv_cudaarithm:BOOL="0" -DBUILD_PERF_TESTS:BOOL="0" -DBUILD_opencv_cudalegacy:BOOL="0" -DBUILD_opencv_cudaimgproc:BOOL="0" -DBUILD_opencv_cudastereo:BOOL="0" -DBUILD_opencv_cudafilters:BOOL="0" -DBUILD_opencv_cudabgsegm:BOOL="0" -DBUILD_SHARED_LIBS:BOOL="0" -DWITH_ITT=OFF ../
+        make -j 32
+        cd -
+        cd opencv-4.1.0/cmake_static
+        cmake -DWITH_OPENEXR=OFF -DOPENCV_GENERATE_PKGCONFIG=ON BUILD_JASPER=ON -DBUILD_JPEG:BOOL="1" -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_COMPILER=$ARM64_GCC_PATH/bin/aarch64-none-linux-gnu-gcc -DCMAKE_CXX_COMPILER=$ARM64_GCC_PATH/bin/aarch64-none-linux-gnu-g++ -DCMAKE_TOOLCHAIN_FILE=../platforms/linux/arm-gnueabi.toolchain.cmake ../
+        make -j32
+        cd -
+    fi
+}
+
+download_compile_armnn(){
+    #requires tflite2.8 to be build first
+    cd $HOME
+    export BASEDIR=~/ArmNNDelegate
+    mkdir $BASEDIR
+    apt-get update && apt-get install git wget unzip zip python git cmake scons
+
+    git clone "https://review.mlplatform.org/ml/armnn" 
+    cd armnn
+    git checkout branches/armnn_22_02
+    
+    #Buikd compute lib
+    cd $BASEDIR
+    git clone https://review.mlplatform.org/ml/ComputeLibrary 
+    cd ComputeLibrary/
+    git checkout $(../armnn/scripts/get_compute_library.sh -p) # e.g. v21.11
+    # The machine used for this guide only has a Neon CPU which is why I only have "neon=1" but if 
+    # your machine has an arm Gpu you can enable that by adding `opencl=1 embed_kernels=1 to the command below
+    sed -i 's/aarch64-linux-gnu-/aarch64-none-linux-gnu-/' SConstruct
+    scons arch=arm64-v8a neon=1 extra_cxx_flags="-fPIC" benchmark_tests=0 validation_tests=0 
+
+    
+    #compile protobuff
+    cd $BASEDIR
+    git clone -b v3.12.0 https://github.com/google/protobuf.git protobuf
+    cd protobuf
+    git submodule update --init --recursive
+    ./autogen.sh
+    mkdir arm64_build
+    cd arm64_build
+    CC=aarch64-none-linux-gnu-gcc \
+    CXX=aarch64-none-linux-gnu-g++ \
+    ../configure --host=aarch64-linux \
+    --prefix=$BASEDIR/google/arm64_pb_install \
+    --with-protoc=$BASEDIR/google/x86_64_pb_install/bin/protoc
+    make install -j16
+    
+    #build flatbuffers
+    cd $BASEDIR
+    wget -O flatbuffers-1.12.0.tar.gz https://github.com/google/flatbuffers/archive/v1.12.0.tar.gz
+    tar xf flatbuffers-1.12.0.tar.gz
+    cd flatbuffers-1.12.0
+    rm -f CMakeCache.txt
+    mkdir build-arm64
+    cd build-arm64
+    # Add -fPIC to allow us to use the libraries in shared objects.
+    CXXFLAGS="-fPIC" cmake .. -DCMAKE_C_COMPILER=aarch64-none-linux-gnu-gcc \
+        -DCMAKE_CXX_COMPILER=aarch64-none-linux-gnu-g++ \
+        -DFLATBUFFERS_BUILD_FLATC=1 \
+        -DCMAKE_INSTALL_PREFIX:PATH=$BASEDIR/flatbuffers-arm64 \
+        -DFLATBUFFERS_BUILD_TESTS=0
+    make all install
+    
+    #finally build armnn
+    cd $BASEDIR
+    cd armnn
+    rm -rf build # Remove any previous cmake build.
+    mkdir build && cd build
+    # if you've got an arm Gpu add `-DARMCOMPUTECL=1` to the command below
+    CXX=aarch64-none-linux-gnu-g++
+    CC=aarch64-none-linux-gnu-gcc
+    cmake .. -DARMCOMPUTE_ROOT=$BASEDIR/ComputeLibrary \
+        -DARMCOMPUTE_BUILD_DIR=$BASEDIR/ComputeLibrary/build/ \
+        -DARMCOMPUTENEON=1 \
+        -DARMCOMPUTECL=1 \
+        -DARMNNREF=1 \
+        -DBUILD_TF_LITE_PARSER=1 \
+        -DTENSORFLOW_ROOT=$HOME/tensorflow/tensorflow_src \
+        -DFLATBUFFERS_ROOT=$BASEDIR/flatbuffers-1.12.0/build-arm64 \
+        -DFLATC_DIR=$BASEDIR/flatbuffers-1.12.0/build \
+        -DPROTOBUF_ROOT=$BASEDIR/google/x86_64_pb_install \
+        -DPROTOBUF_ROOT=$BASEDIR/google/x86_64_pb_install/ \
+        -DPROTOBUF_LIBRARY_DEBUG=$BASEDIR/google/arm64_pb_install/lib/libprotobuf.so.23.0.0 \
+        -DPROTOBUF_LIBRARY_RELEASE=$BASEDIR/google/arm64_pb_install/lib/libprotobuf.so.23.0.0 \
+        -DTF_LITE_SCHEMA_INCLUDE_PATH=$HOME/tensorflow/tensorflow_src/tensorflow/lite/schema \
+        -DTFLITE_LIB_ROOT=$HOME/tensorflow/tflite_build_arm \
+        -DBUILD_ARMNN_TFLITE_DELEGATE=1 
+    make
+}
+
 SCRIPTDIR=`pwd`
 
 
@@ -52,7 +184,6 @@ case $key in
     echo Options,
     echo --skip_cpp_deps            Skip Downloading or Compiling dependencies for CPP examples
     echo --skip_arm_gcc_download            Skip Downloading or setting environment variable  for ARM64_GCC_PATH
-    
     exit 0
     ;;
 esac
@@ -61,10 +192,9 @@ done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 
-
 version_match=`python3 -c 'import sys;r=0 if sys.version_info >= (3,6) else 1;print(r)'`
 if [ $version_match -ne 0 ]; then
-echo 'python version must be >= 3.6'
+    echo 'python version must be >= 3.6'
 return
 fi
 
@@ -75,8 +205,8 @@ elif [[ $arch == aarch64 ]]; then
     echo "ARM Architecture"
     $skip_arm_gcc_download=1
 else
-echo 'Processor Architecture must be x86_64 or aarch64'
-echo 'Processor Architecture "'$arch'" is Not Supported '
+    echo 'Processor Architecture must be x86_64 or aarch64'
+    echo 'Processor Architecture "'$arch'" is Not Supported '
 return
 fi
 
@@ -84,71 +214,55 @@ fi
 # Installing dependencies
 echo 'Installing python packages...'
 if [[ $arch == x86_64 ]]; then
-pip3 install -r ./requirements_pc.txt
+    pip3 install -r ./requirements_pc.txt
 elif [[ $arch == aarch64 ]]; then
-pip3 install -r ./requirements_j7.txt
+    pip3 install -r ./requirements_j7.txt
 fi
-
-if [[ -z "$TIDL_TOOLS_PATH" ]]
-then
-wget https://github.com/TexasInstruments/edgeai-tidl-tools/releases/download/08_02_00_01-rc1/tidl_tools.tar.gz
-tar -xzf tidl_tools.tar.gz
-rm tidl_tools.tar.gz
-cd  tidl_tools
-export TIDL_TOOLS_PATH=$(pwd)
-cd ..
-fi
-
-if [ $skip_cpp_deps -eq 0 ]
-then
-if [[ $arch == x86_64 ]]; then
-    cd  $TIDL_TOOLS_PATH
-    wget https://github.com/TexasInstruments/edgeai-tidl-tools/releases/download/08_02_00_01-rc1/libonnxruntime.so.1.7.0
-    wget https://github.com/TexasInstruments/edgeai-tidl-tools/releases/download/08.00.00-rc2/libtensorflow-lite.a
-    ln -s libonnxruntime.so.1.7.0 libonnxruntime.so
-    cd -
-fi
+if [[ -z "$TIDL_TOOLS_PATH" ]]; then
+    wget https://github.com/TexasInstruments/edgeai-tidl-tools/releases/download/08_02_00_01-rc1/tidl_tools.tar.gz
+    tar -xzf tidl_tools.tar.gz
+    rm tidl_tools.tar.gz
+    cd  tidl_tools
+    export TIDL_TOOLS_PATH=$(pwd)
+    cd ..
 fi
 
 
-if [[ $arch == x86_64 ]]; then
-export LD_LIBRARY_PATH=$TIDL_TOOLS_PATH
+
+if [ $skip_arm_gcc_download -eq 0 ]; then
+    wget https://developer.arm.com/-/media/Files/downloads/gnu-a/9.2-2019.12/binrel/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu.tar.xz
+    tar -xf gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu.tar.xz
+    export ARM64_GCC_PATH=$(pwd)/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu
 fi
 
-
-if [ $skip_arm_gcc_download -eq 0 ]
-then
-wget https://developer.arm.com/-/media/Files/downloads/gnu-a/9.2-2019.12/binrel/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu.tar.xz
-tar -xf gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu.tar.xz
-export ARM64_GCC_PATH=$(pwd)/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu
-fi
-
-if [ $skip_cpp_deps -eq 0 ]
-then
-cd $HOME
-git clone --depth 1 --single-branch -b tidl-j7 https://github.com/TexasInstruments/neo-ai-dlr
-cd neo-ai-dlr
-git submodule init
-git submodule update --init --recursive
-cd ..
-git clone --depth 1 --single-branch -b tidl-j7 https://github.com/TexasInstruments/onnxruntime.git
-git clone --depth 1 --single-branch -b tidl-j7 https://github.com/TexasInstruments/tensorflow.git
-mkdir -p tensorflow/tensorflow/lite/tools/make/downloads
-cd tensorflow/tensorflow/lite/tools/make/downloads
-wget https://github.com/google/flatbuffers/archive/v1.12.0.tar.gz
-tar -xzf v1.12.0.tar.gz
-rm v1.12.0.tar.gz
-mv  flatbuffers-1.12.0 flatbuffers
-cd -
-wget https://github.com/opencv/opencv/archive/4.1.0.zip
-unzip 4.1.0.zip
-rm 4.1.0.zip
-if [[ $arch == x86_64 ]]; then
-cd opencv-4.1.0/cmake/
-cmake -DBUILD_opencv_highgui:BOOL="1" -DBUILD_opencv_videoio:BOOL="0" -DWITH_IPP:BOOL="0" -DWITH_WEBP:BOOL="1" -DWITH_OPENEXR:BOOL="1" -DWITH_IPP_A:BOOL="0" -DBUILD_WITH_DYNAMIC_IPP:BOOL="0" -DBUILD_opencv_cudacodec:BOOL="0" -DBUILD_PNG:BOOL="1" -DBUILD_opencv_cudaobjdetect:BOOL="0" -DBUILD_ZLIB:BOOL="1" -DBUILD_TESTS:BOOL="0" -DWITH_CUDA:BOOL="0" -DBUILD_opencv_cudafeatures2d:BOOL="0" -DBUILD_opencv_cudaoptflow:BOOL="0" -DBUILD_opencv_cudawarping:BOOL="0" -DINSTALL_TESTS:BOOL="0" -DBUILD_TIFF:BOOL="1" -DBUILD_JPEG:BOOL="1" -DBUILD_opencv_cudaarithm:BOOL="0" -DBUILD_PERF_TESTS:BOOL="0" -DBUILD_opencv_cudalegacy:BOOL="0" -DBUILD_opencv_cudaimgproc:BOOL="0" -DBUILD_opencv_cudastereo:BOOL="0" -DBUILD_opencv_cudafilters:BOOL="0" -DBUILD_opencv_cudabgsegm:BOOL="0" -DBUILD_SHARED_LIBS:BOOL="0" -DWITH_ITT=OFF ../
-make -j 32
-cd -
-fi
+if [ $skip_cpp_deps -eq 0 ]; then
+    if [[ $arch == x86_64 ]]; then
+        cd  $TIDL_TOOLS_PATH
+        wget https://github.com/TexasInstruments/edgeai-tidl-tools/releases/download/08_02_00_01-rc1/libonnxruntime.so.1.7.0
+        wget https://github.com/TexasInstruments/edgeai-tidl-tools/releases/download/08.00.00-rc2/libtensorflow-lite.a
+        ln -s libonnxruntime.so.1.7.0 libonnxruntime.so
+        export LD_LIBRARY_PATH=$TIDL_TOOLS_PATH
+        cd -
+    fi
+    cd $HOME
+    git clone --depth 1 --single-branch -b tidl-j7 https://github.com/TexasInstruments/neo-ai-dlr
+    cd neo-ai-dlr
+    git submodule init
+    git submodule update --init --recursive
+    cd ..
+    git clone --depth 1 --single-branch -b tidl-j7 https://github.com/TexasInstruments/onnxruntime.git
+    if [[ $device == j7 ]]; then
+        echo "Device j7: Downloading tflite 2.4"
+        download_tflite2.4
+    elif [[  $device == am62 ]]; then
+        echo "Device am62: Downloading tflite 2.8"
+        download_compile_tflite_2.8
+    else
+        echo 'unknown device: "'$device'"'
+        return
+    fi
+    cd $HOME
+    download_compile_opencv
 fi
 
 cd $SCRIPTDIR
