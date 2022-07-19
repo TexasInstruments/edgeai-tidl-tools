8212504952a4a9e40fe3ffd9ed1178017ca3cf0b
Fadil Muhammed <a0496663@tidl-ud-03>
2022-07-19 11:28:53 +0530

    minor comment change

--- dockers/qemu/dlr_prepare.sh
+++ dockers/qemu/dlr_prepare.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+# This script should be run inside the docker host machine 
+# Outputs:
+# - This script will clone required files for corresponding xxx_build.sh file 
+
 mkdir dlr
 cd dlr
 git clone --depth 1 --single-branch -b master  https://git.ti.com/git/jacinto-linux/meta-psdkla.git
--- dockers/qemu/onnxrt_prepare.sh
+++ dockers/qemu/onnxrt_prepare.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+# This script should be run inside the docker host machine 
+# Outputs:
+# - This script will clone required files for corresponding xxx_build.sh file 
+
 mkdir onnx
 cd onnx
 
