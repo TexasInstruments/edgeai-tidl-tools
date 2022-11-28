126c1beafa30e320adc606dad090b762ffa79f1c
Akash Thatavarthi <a0503500@tidl-ud-01>
2022-11-28 12:12:52 +0530

    [ADDED NEW OPTIONS] added new options such as denylist:layer_type and denylist:layer_name

--- examples/osrt_python/common_utils.py
+++ examples/osrt_python/common_utils.py
@@ -62,7 +62,9 @@ optional_options = {
 "tensor_bits":tensor_bits,
 "debug_level":debug_level,
 "max_num_subgraphs":max_num_subgraphs,
-"deny_list":"", #"MaxPool",
+"deny_list":"", #"MaxPool","1,43","GlobalAveragePool" 
+"deny_list:layer_type":"GlobalAveragePool,Flatten", #"MaxPool',"MaxPool,GlobalAveragePool","1,43",'GlobalAveragePool,Flatten"
+"deny_list:layer_name":"",#"GlobalAveragePool_97,Flatten_98", "19,18,22","MobilenetV2/expanded_conv_2d/add"
 "accuracy_level":accuracy_level,
 "advanced_options:calibration_frames": calibration_frames,
 "advanced_options:calibration_iterations": calibration_iterations,
