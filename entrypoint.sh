7cfc84626402f1c1c041aec708647c533a4fa035
Fadil Muhammed <a0496663@tidl-ud-03>
2023-02-08 11:15:00 +0530

    clean copy

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@ echo "Current time is $time" > ~/report.txt
 cd /root/edgeai-tidl-tools
 rm -r tidl_tools
 rm -r tidl_tools.tar.gz
+rm output_images/* 
+rm -r model-artifacts/*
 pwd
 if [ -z "$LOCAL_PATH" ];then
     source ./setup.sh --skip_x86_python_install
@@ -21,9 +23,12 @@ source ./scripts/run_python_examples.sh
 python3 ./scripts/gen_test_report.py
 mkdir -p test_reports/$SOC/output_images
 mkdir -p test_reports/$SOC/model-artifacts
-mv output_images/* test_reports/$SOC/output_images
-mv test_report_pc.csv test_reports/$SOC/
-mv model-artifacts/* test_reports/$SOC/model-artifacts
+rm test_reports/$SOC/output_images/*
+rm test_reports/$SOC/test_report_pc.csv
+rm -r test_reports/$SOC/model-artifacts/*
+cp output_images/* test_reports/$SOC/output_images/
+cp test_report_pc.csv test_reports/$SOC/
+cp -r  model-artifacts/* test_reports/$SOC/model-artifacts/
 mv ~/report.txt test_reports/$SOC/
 # ls -l
 
