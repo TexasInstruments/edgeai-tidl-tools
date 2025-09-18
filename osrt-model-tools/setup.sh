32aa8ca302734d9091bed3f992f8255c622af3d1
Abhay Chirania <a-chirania@ti.com>
2025-09-18 20:34:08 +0530

    [OSRT_MODEL_TOOLS] Install minimal version on nvidia-graphsurgeon
    Signed-off-by: Abhay Chirania <a-chirania@ti.com>

--- osrt-model-tools/setup.sh
+++ osrt-model-tools/setup.sh
@@ -34,13 +34,14 @@
 # upgrade pip
 pip install --no-input --upgrade pip setuptools
 
-
 echo 'Installing python packages...'
 while IFS= read -r line;
 do
 	echo "pip3 install --no-input $line";
 	pip3 install --no-input $line;
 done < requirements.txt
+
+pip install --no-input onnx-graphsurgeon==0.3.26 --extra-index-url https://pypi.ngc.nvidia.com
 
 echo 'installing the onnx graph optimization toolkit...'
 python3 ./setup.py develop
