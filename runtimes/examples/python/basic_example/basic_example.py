e7cd0ce0b82a9f40aa21e9f93ca14ebf82f0d197
Abhay-Chirania <a-chirania@ti.com>
2026-02-26 17:27:42 +0530

    [EXAMPLES] basic_example.py: Fix import of onnxruntime for disabling graph optimization
    Signed-off-by: Abhay-Chirania <a-chirania@ti.com>

--- runtimes/examples/python/basic_example/basic_example.py
+++ runtimes/examples/python/basic_example/basic_example.py
@@ -289,6 +289,7 @@ def run(config,
         # ONNXRT
         if info["runtime"] == "onnxrt":
             from onnxrt.onnxrt_wrapper import ONNXRT
+            import onnxruntime
             session = ONNXRT(model_path=info["path"], tidl_offload=not disable_tidl_offload)
 
             '''
